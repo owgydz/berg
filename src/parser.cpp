babde4db2f1591020a059ae6f34a45e61f95f9fd
Owen Gaydosz <owgydz@proton.me>
2025-03-07 16:17:30 -0500

    cleanup parser.cpp

--- src/parser.cpp
+++ src/parser.cpp
@@ -1,6 +1,4 @@
-// Parser for .berg files
-
-#include "../bergh/parser.h"
+#include "parser.h"
 #include <fstream>
 #include <sstream>
 
@@ -12,16 +10,35 @@ bool BergParser::parseFile(const std::string& filename) {
     while (std::getline(file, line)) {
         size_t pos = line.find(':');
         if (pos == std::string::npos) continue;
-        
+
         std::string key = line.substr(0, pos);
         std::string value = line.substr(pos + 1);
-        data[key] = value;
+        if (key == "target") {
+            target = value;
+        } else if (key == "source") {
+            std::stringstream ss(value);
+            std::string source;
+            while (ss >> source) {
+                sources.push_back(source);
+            }
+        } else if (key == "command") {
+            command = value;
+        }
     }
 
     return true;
 }
 
 std::string BergParser::getValue(const std::string& key) const {
-    auto it = data.find(key);
-    return it != data.end() ? it->second : "";
+    if (key == "target") return target;
+    if (key == "command") return command;
+    return "";
+}
+
+std::vector<std::string> BergParser::getSources() const {
+    return sources;
+}
+
+std::string BergParser::getCommand() const {
+    return command;
 }
