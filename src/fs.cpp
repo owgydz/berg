0593376b82a8fae48a56275c782ee0382512437e
Owen Gaydosz <owgydz@proton.me>
2025-03-07 16:16:31 -0500

    fs.cpp

---
+++ src/fs.cpp
@@ -0,0 +1,16 @@
+#include "fs.h"
+#include <sys/stat.h>
+#include <stdexcept>
+
+bool fileExists(const std::string& filename) {
+    struct stat buffer;
+    return (stat(filename.c_str(), &buffer) == 0);
+}
+
+time_t getFileModificationTime(const std::string& filename) {
+    struct stat buffer;
+    if (stat(filename.c_str(), &buffer) != 0) {
+        throw std::runtime_error("File not found: " + filename);
+    }
+    return buffer.st_mtime;
+}
