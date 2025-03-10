f7e16d57c94ab4225e85ac1c12422efb0d16a457
Owen Gaydosz <owgydz@proton.me>
2025-03-10 18:13:53 -0400

    Update parser.h to handle .bergi files

--- bergh/parser.h
+++ bergh/parser.h
@@ -9,9 +9,18 @@ public:
     std::string getValue(const std::string& key) const;
     std::vector<std::string> getSources() const;
     std::string getCommand() const;
+    bool optionExists(const std::string& option) const;
+    std::string getOption(const std::string& option) const;
 
 private:
+    void importBergi(const std::string& bergiFile);
+    void handleOption(const std::string& optionName);
+    void parseKeyValue(const std::string& key, const std::string& value);
+    std::string trimWhitespace(const std::string& str) const;
+
     std::string target;
     std::vector<std::string> sources;
     std::string command;
+    std::unordered_map<std::string, std::string> keyValues;
+    std::unordered_map<std::string, std::string> options;
 };
